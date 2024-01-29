210218bed31ad2275b6ab5c78f45c5aea62be730
Tlotlo <114500548+butterrobot2022@users.noreply.github.com>
2024-01-29 14:42:30 +0200

    new commit

--- Frontend/src/components/side_navs.jsx
+++ Frontend/src/components/side_navs.jsx
@@ -14,8 +14,8 @@ export default function SideNavs() {
                 <Link to="/market_makers" className="side-nav"><button className="btn btn-light side-nav-btn"><p><i className="bi bi-journal-bookmark-fill"></i> Market Makers</p></button></Link>
                 <Link to={`/conversation/${uniqueID}`} className="side-nav"><button className="btn btn-light side-nav-btn"><p><i class="bi bi-chat-square-dots"></i> ChatBot</p></button></Link>
                 {/* <Link to="#" className="side-nav"><p><i className="bi bi-book"></i>Books</p></Link> */}
-                <Link to="/update_news" className="side-nav"><button className="btn btn-light side-nav-btn"><p><i class="bi bi-info-circle-fill"></i> Update News</p></button></Link>
-                <Link to="/all_trades" className="side-nav"><button className="btn btn-light side-nav-btn"><p><i className="bi bi-newspaper"></i> Enter Trades</p></button></Link>
+                <Link to="/update_news" className="side-nav"><button className="btn btn-light side-nav-btn"><p><i class="bi bi-newspaper"></i> Update News</p></button></Link>
+                <Link to="/all_trades" className="side-nav"><button className="btn btn-light side-nav-btn"><p><i className="bi bi-info-circle-fill"></i> Enter Trades</p></button></Link>
                 <Link to="/models" className="side-nav"><button className="btn btn-light side-nav-btn"><p><i className="bi bi-robot"></i> Create Models</p></button></Link>
                 {/* <Link to="/payment" className="side-nav"><i class="bi bi-credit-card-fill"></i>Market Dictionary</Link> */}
         </div>
@@ -27,8 +27,8 @@ export default function SideNavs() {
                 {/* <Link to="/all_journals" className="side-nav"><i className="bi bi-journal-bookmark-fill"></i></Link> */}
                 <Link to={`/conversation/${uniqueID}`} className="side-nav"><i className="bi bi-chat-square-dots"></i></Link>
                 {/* <Link to="#" className="side-nav"><i className="bi bi-book"></i></Link> */}
-                <Link to="/update_news" className="side-nav"><i class="bi bi-info-circle-fill"></i></Link>
-                <Link to="/all_trades" className="side-nav"><i className="bi bi-newspaper"></i></Link>
+                <Link to="/update_news" className="side-nav"><i class="bi bi-newspaper"></i></Link>
+                <Link to="/all_trades" className="side-nav"><i className="bi bi-info-circle-fill"></i></Link>
                 <Link to="/models" className="side-nav"><i className="bi bi-robot"></i></Link>
         </div>
         </div>
