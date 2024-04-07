01425242981dca5863e3f7c94dcd76650cbdb14d
Tlotlo <114500548+butterrobot2022@users.noreply.github.com>
2024-04-07 17:57:31 +0200

    new commit

--- Frontend/src/components/legodi_login.jsx
+++ Frontend/src/components/legodi_login.jsx
@@ -1,45 +1,114 @@
-import React, { useEffect, useState } from "react";
-import Header from "./header";
-import SideNavs from "./side_navs";
-import Cookies from 'js-cookie';
-import { useNavigate } from "react-router-dom";
-import Logo from '../images/legodi_logo.jpg';
+// src/components/LegodiLogin.js
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-
+import { useNavigate } from "react-router-dom";
+import axios from "axios"; // Import Axios or use the fetch API directly
 
 export default function LegodiLogin() {
-    return (
-        <div><br /><br /><br />
-            <div className="containers">
-    <input type="checkbox" id="flip" />
+
+    const baseUrl = 'https://backend-production-c0ab.up.railway.app';
+
+    const [submitButton, setSubmitButton] = useState('Submit Button');
+
+    const navigate = useNavigate();
     
-    <div className="forms">
-        <div className="form-content">
-          
-        <div className="login-form">
-            <div className="title">Login</div>
-          <form action="#">
-            <div className="input-boxes">
-              <div className="input-box">
-                <i className="fas fa-envelope"></i>
-                <input type="text" placeholder="Enter your email" required />
-              </div>
-              <div className="input-box">
-                <i className="fas fa-lock"></i>
-                <input type="password" placeholder="Enter your password" required />
-              </div>
-              <div className="button input-box">
-                <input type="submit" value="Sumbit" />
-              </div>
-              <div className="text sign-up-text">
-                <Link to="/regr">Don't have an account? Sigup now</Link>
-              </div>
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+    });
+
+    function getCookie(name) {
+        const value = `; ${document.cookie}`;
+        const parts = value.split(`; ${name}=`);
+        if (parts.length === 2) return parts.pop().split(';').shift();
+    }
+    
+
+    const handleSubmit = async (e) => {
+        setSubmitButton('Logging in...');
+        e.preventDefault();
+        try {
+            // Get the CSRF token from the cookie (assuming it's named 'csrftoken')
+            const csrfToken = getCookie('csrftoken'); // Implement the getCookie function
+
+            console.log('CSRF Token');
+            console.log(csrfToken);
+    
+            // Make a POST request to your login endpoint
+            const response = await axios.post(`${baseUrl}/api/login/`, formData, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-CSRFToken': csrfToken,
+                },
+            });
+            
+
+            const responseData = response.data;
+            console.log(responseData);
+            
+
+            navigate('/map');
+    
+            // Store the token (e.g., in local storage or state)
+            // Redirect to the desired page (e.g., dashboard)
+        } catch (error) {
+            console.error('Login failed:', error.response.data);
+            alert('Error occurred. Please try again.');
+            setSubmitButton('Submit');
+            // Handle error (e.g., display error message to the user)
+        }
+        setSubmitButton('Submit');
+    };
+    
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    return (
+        <div>
+            <br /><br /><br />
+            <div className="containers">
+                <input type="checkbox" id="flip" />
+                <div className="forms">
+                    <div className="form-content">
+                        <div className="login-form">
+                            <div className="title">Login</div>
+                            <form onSubmit={handleSubmit}>
+                                <div className="input-boxes">
+                                    <div className="input-box">
+                                        <i className="fas fa-envelope"></i>
+                                        <input
+                                            type="email"
+                                            name="email"
+                                            placeholder="Enter your email"
+                                            onChange={handleChange}
+                                            required
+                                        />
+                                    </div>
+                                    <div className="input-box">
+                                        <i className="fas fa-lock"></i>
+                                        <input
+                                            type="password"
+                                            name="password"
+                                            placeholder="Enter your password"
+                                            onChange={handleChange}
+                                            required
+                                        />
+                                    </div>
+                                    <div className="button input-box">
+                                        <input type="submit" value="Submit" />
+                                    </div>
+                                    <div className="text sign-up-text">
+                                        <Link to="/regr">
+                                            Don't have an account? Signup now
+                                        </Link>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </form>
-      </div>
-    </div>
-    </div>
-  </div>
-  </div>
-    )
+        </div>
+    );
 }
--- Frontend/src/components/legodi_registration.jsx
+++ Frontend/src/components/legodi_registration.jsx
@@ -1,48 +1,101 @@
-import React, { useEffect, useState } from "react";
-import Header from "./header";
-import SideNavs from "./side_navs";
-import Cookies from 'js-cookie';
-import { useNavigate } from "react-router-dom";
-import Logo from '../images/legodi_logo.jpg';
+// src/components/LegodiRegistration.js
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import axios from "axios"; // Import Axios or use the fetch API directly
 
 export default function LegodiRegistration() {
+
+    const baseUrl = 'https://backend-production-c0ab.up.railway.app';
+
+    const navigate = useNavigate();
+
+    const [formData, setFormData] = useState({
+        username: "",
+        email: "",
+        password: "",
+    });
+
+    const [submitButton, setSubmitButton] = useState('Submit');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setSubmitButton('Submitting Details...')
+        try {
+            // Make a POST request to your backend API endpoint
+            const response = await axios.post(`${baseUrl}/api/register/`, formData);
+            console.log("User registered successfully:", response.data);
+            setSubmitButton('Submit');
+            navigate('/legodi-login');
+            // Redirect to login page or show success message to the user
+        } catch (error) {
+            console.error("Registration failed:", error.response.data);
+            alert('Error occured. Please make sure you have filled in all the details.')
+            setSubmitButton('Submit');
+            // Handle error (e.g., display error message to the user)
+        }
+        setSubmitButton('Submit');
+    };
+
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
     return (
-        <div><br /><br /><br />
+        <div>
+            <br /><br /><br />
             <div className="containers">
-    <input type="checkbox" id="flip" />
-    
-    <div className="forms">
-        <div className="form-content">
-          
-        <div className="signup-form">
-          <div className="title">Signup</div>
-        <form action="#">
-            <div className="input-boxes">
-              <div className="input-box">
-                <i className="fas fa-user"></i>
-                <input type="text" placeholder="Enter your name" required />
-              </div>
-              <div className="input-box">
-                <i className="fas fa-envelope"></i>
-                <input type="text" placeholder="Enter your email" required />
-              </div>
-              <div className="input-box">
-                <i className="fas fa-lock"></i>
-                <input type="password" placeholder="Enter your password" required />
-              </div>
-              <div className="button input-box">
-                <input type="submit" value="Sumbit" />
-              </div>
-              <div className="text sign-up-text">
-                  <Link to="/legodi-login">Already have an account? Login here</Link>
-              </div>
+                <input type="checkbox" id="flip" />
+                <div className="forms">
+                    <div className="form-content">
+                        <div className="signup-form">
+                            <div className="title">Signup</div>
+                            <form onSubmit={handleSubmit}>
+                                <div className="input-boxes">
+                                    <div className="input-box">
+                                        <i className="fas fa-user"></i>
+                                        <input
+                                            type="text"
+                                            name="username"
+                                            placeholder="Enter your name and surname"
+                                            onChange={handleChange}
+                                            required
+                                        />
+                                    </div>
+                                    <div className="input-box">
+                                        <i className="fas fa-envelope"></i>
+                                        <input
+                                            type="text"
+                                            name="email"
+                                            placeholder="Enter your email"
+                                            onChange={handleChange}
+                                            required
+                                        />
+                                    </div>
+                                    <div className="input-box">
+                                        <i className="fas fa-lock"></i>
+                                        <input
+                                            type="password"
+                                            name="password"
+                                            placeholder="Enter your password"
+                                            onChange={handleChange}
+                                            required
+                                        />
+                                    </div>
+                                    <div className="button input-box">
+                                        <input type="submit" value={submitButton} onClick={handleSubmit} />
+                                    </div>
+                                    <div className="text sign-up-text">
+                                        <Link to="/legodi-login">
+                                            Already have an account? Login here
+                                        </Link>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
-      </form>
-    </div>
-    </div>
-    </div>
-  </div>
-  </div>
-    )
+        </div>
+    );
 }
