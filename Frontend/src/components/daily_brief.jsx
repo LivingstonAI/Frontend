b4b1ae95f210fb9d3ac1b139d0ce95a17b18064c
Tlotlo <114500548+butterrobot2022@users.noreply.github.com>
2024-07-26 08:57:21 +0200

    new commit

--- Frontend/src/components/daily_brief.jsx
+++ Frontend/src/components/daily_brief.jsx
@@ -5,6 +5,7 @@ import Cookies from 'js-cookie';
 
 export default function DailyBrief() {
     const [dailyBriefData, setDailyBriefData] = useState([]);
+    const [filter, setFilter] = useState("");
     const baseUrl = 'https://backend-production-c0ab.up.railway.app';
 
     useEffect(() => {
@@ -20,6 +21,11 @@ export default function DailyBrief() {
         .catch(error => console.error('Error fetching daily brief data:', error));
     }, []);
 
+    // Filter the briefings based on the filter input
+    const filteredBriefData = dailyBriefData.filter(brief =>
+        brief.asset.toLowerCase().includes(filter.toLowerCase())
+    );
+
     return (
         <div>
             <div className="header">
@@ -29,10 +35,19 @@ export default function DailyBrief() {
                 <SideNavs />
                 <div className="main-body-info">
                     <h5>Daily Brief</h5>
+                    <div className="daily-brief-asset-filter">
+                        <input
+                            type="text"
+                            placeholder="Filter by Asset"
+                            className="filter-input form-control"
+                            value={filter}
+                            onChange={(e) => setFilter(e.target.value)}
+                        /><br />
+                    </div>
                     <hr />
                     <div className="daily-brief-div">
-                        {dailyBriefData.length > 0 ? (
-                            dailyBriefData.map((brief, index) => (
+                        {filteredBriefData.length > 0 ? (
+                            filteredBriefData.map((brief, index) => (
                                 <div key={index}>
                                     <p><b>Asset</b>: {brief.asset}</p>
                                     <p><b>Summary</b>: <br />{brief.summary}</p>
