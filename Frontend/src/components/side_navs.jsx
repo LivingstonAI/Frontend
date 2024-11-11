d0eee395d3308ea2995a120335016e11c46bc2bf
Tlotlo <114500548+butterrobot2022@users.noreply.github.com>
2024-11-11 07:00:34 +0200

    new commit

--- Frontend/src/components/side_navs.jsx
+++ Frontend/src/components/side_navs.jsx
@@ -52,7 +52,7 @@ export default function SideNavs() {
     return(
         <div className="all-side-navs">
         <div className="side-navs trading-history-links">
-                <Link to="/personal_info" className="side-nav"><button className="btn btn-light side-nav-btn"><p><i className="bi bi-person"></i>P Info</p></button></Link>
+                <Link to="/personal_info" className="side-nav"><button className="btn btn-light side-nav-btn"><p><i className="bi bi-person-fill"></i>P Info</p></button></Link>
               <Link to="/trading_history_analytics" className="side-nav"><button className="btn btn-light side-nav-btn"><p><i className="bi bi-bar-chart-line-fill"></i> Analytics</p></button></Link>
                 <Link to="/market_makers" className="side-nav"><button className="btn btn-light side-nav-btn"><p><i className="bi bi-bank"></i> Macros</p></button></Link>
                 <Link to={`/conversation/${uniqueID}`} className="side-nav"><button className="btn btn-light side-nav-btn"><p><i class="bi bi-chat-square-dots"></i> ChatBot</p></button></Link>
@@ -70,16 +70,16 @@ export default function SideNavs() {
         </div>
         <div className="side-navs-cellphone">
                 {/* <Link to="/overview" className="side-nav"><i className="bi bi-plus-lg"></i></Link> */}
-                <Link to="/personal_info" className="side-nav"><i className="bi bi-person"></i></Link>
-                <Link to="/trading_history_analytics" className="side-nav"><i className="bi bi-bar-chart-line"></i></Link>
+                <Link to="/personal_info" className="side-nav"><i className="bi bi-person-fill"></i></Link>
+                <Link to="/trading_history_analytics" className="side-nav"><i className="bi bi-bar-chart-line-fill"></i></Link>
                 <Link to="/market_makers" className="side-nav"><i className="bi bi-bank" /></Link>
                 {/* <Link to="/all_journals" className="side-nav"><i className="bi bi-journal-bookmark-fill"></i></Link> */}
                 <Link to={`/conversation/${uniqueID}`} className="side-nav"><i className="bi bi-chat-square-dots"></i></Link>
                 {/* <Link to="#" className="side-nav"><i className="bi bi-book"></i></Link> */}
-                <Link to='/daily_brief' className="side-nav"><i className="bi bi-briefcase"></i></Link>
+                <Link to='/daily_brief' className="side-nav"><i className="bi bi-briefcase-fill"></i></Link>
                 <Link to='/performance_review/asset' className="side-nav"><i className="bi bi-journal-bookmark"></i></Link>
                 <Link to="/update_news" className="side-nav"><i class="bi bi-newspaper"></i></Link>
-                <Link to="/all_trades" className="side-nav"><i className="bi bi-info-circle"></i></Link>
+                <Link to="/all_trades" className="side-nav"><i className="bi bi-info-circle-fill"></i></Link>
                 <Link to="/scratch" className="side-nav"><i className="bi bi-robot"></i></Link>
                 <Link to="/model_performance" className="side-nav"><i className="bi bi-pen"></i></Link>
                 <Link to="/risk_bot" className="side-nav"><i className="bi bi-currency-exchange"></i></Link>
