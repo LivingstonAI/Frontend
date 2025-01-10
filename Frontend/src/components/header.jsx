b8f7143584d0ed00b9ee6dbcd3cd00cddac9c829
Tlotlo <114500548+butterrobot2022@users.noreply.github.com>
2025-01-10 15:49:27 +0200

    Your commit message here

--- Frontend/src/components/header.jsx
+++ Frontend/src/components/header.jsx
@@ -9,7 +9,7 @@ export default function Header() {
             {/* <img src="/Users/motin/Downloads/snowAI.png"></img> */}
             <div className="all-header-navs">
             <div className="header-navigations">
-                <Link to="/" className="overview-link"><h5>snowAI</h5></Link>
+                <Link to="/" className="overview-link"><h5>SnowAI</h5></Link>
                 {/* <Link to="/all_journals" className="overview-link"><h5>Main Page</h5></Link> */}
                 {/* <Link to="/personal_info" className="personal-info-link"><i class="bi bi-person personal-info-link-icon"></i></Link> */}
             </div>
