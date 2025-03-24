4b854169dfac23cfc65ec58b5eef65036276e290
Tlotlo <114500548+butterrobot2022@users.noreply.github.com>
2025-03-24 14:26:41 +0200

    orion space belt

--- Frontend/src/components/side_navs.jsx
+++ Frontend/src/components/side_navs.jsx
@@ -251,7 +251,7 @@ export default function SideNavs() {
         <Link to="/tradergpt_analysis" className="side-nav"><button className="btn btn-light side-nav-btn"><p><i className="bi bi-life-preserver"></i></p></button></Link>
         <Link to="/backtested_results" className="side-nav"><button className="btn btn-light side-nav-btn"><p><i className="bi bi-yin-yang"></i></p></button></Link>
         <Link to="/ideas_section" className="side-nav"><button className="btn btn-light side-nav-btn"><p><i className="bi bi-lightbulb-fill"></i></p></button></Link>
-        <Link to="/call_ai" className="side-nav"><button className="btn btn-light side-nav-btn"><p><i className="bi bi-feather"></i></p></button></Link>
+        <Link to="/call_ai" className="side-nav"><button className="btn btn-light side-nav-btn"><p><i className="bi bi-telephone-fill"></i></p></button></Link>
 
       </div>
 
@@ -311,7 +311,7 @@ export default function SideNavs() {
             <i className="bi bi-lightbulb-fill"></i>
         </Link>
         <Link to="/call_ai" className="side-nav">
-            <i className="bi bi-feather"></i>
+            <i className="bi bi-telephone-fill"></i>
         </Link>
       </div>
       <br />
