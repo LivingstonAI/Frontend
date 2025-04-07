caf6b2fc42d4fe32471e9533bd33492c2f45610f
Tlotlo <114500548+butterrobot2022@users.noreply.github.com>
2025-04-07 14:54:22 +0200

    orion space belt

--- Frontend/src/components/asset_tracker.jsx
+++ Frontend/src/components/asset_tracker.jsx
@@ -169,7 +169,7 @@ const AssetTracker = () => {
       <div className="card-header bg-light">
         <div className="d-flex justify-content-between align-items-center">
           <h5 className="mb-0 text-primary d-flex align-items-center">
-            <FaChartLine className="me-2" /> Asset Tracker
+            <FaChartLine className="me-2" />
           </h5>
           <div>
             <button 
