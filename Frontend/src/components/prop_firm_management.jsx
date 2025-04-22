5198691816916ee83e0f1245e1a7de4521d47d27
Tlotlo <114500548+butterrobot2022@users.noreply.github.com>
2025-04-22 10:07:33 +0200

    orion space belt

--- Frontend/src/components/prop_firm_management.jsx
+++ Frontend/src/components/prop_firm_management.jsx
@@ -604,7 +604,7 @@ export default function PropFirmManagement() {
                     <div className="account-summary-left">
                         <div className="prop-firm-info">
                             {selectedAccount.prop_firm.logo ? (
-                                <img src={`data:image/png;base64,${selectedAccount.prop_firm.logo}`} alt={selectedAccount.prop_firm.name} />
+                                <img src={`data:image/${selectedAccount.prop_firm.logo.split(';')[0].split('/')[1]};base64,${selectedAccount.prop_firm.logo}`} alt={selectedAccount.prop_firm.name} />
 
                             ) : (
                                 <div className="no-logo">{selectedAccount.prop_firm.name.charAt(0)}</div>
