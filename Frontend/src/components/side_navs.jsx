85ee7361cca4d2662687130913afa907c9148446
Tlotlo <114500548+butterrobot2022@users.noreply.github.com>
2025-04-27 11:09:43 +0200

    orion space belt

--- Frontend/src/components/side_navs.jsx
+++ Frontend/src/components/side_navs.jsx
@@ -243,7 +243,7 @@ const handlePlay = (songUrl) => {
         <Link to="/call_ai" className="side-nav"><button className="btn btn-light side-nav-btn"><p><i className="bi bi-telephone-fill"></i></p></button></Link>
         <Link to="/trade_ideas" className="side-nav"><button className="btn btn-light side-nav-btn"><p><i className="bi bi-snow2"></i></p></button></Link>
         <Link to="/prop_firm_management" className="side-nav"><button className="btn btn-light side-nav-btn"><p><i className="bi bi-currency-dollar"></i></p></button></Link>
-        <Link to="/art" className="side-nav"><button className="btn btn-light side-nav-btn"><p><i className="bi bi-currency-dollar"></i></p></button></Link>
+        <Link to="/art" className="side-nav"><button className="btn btn-light side-nav-btn"><p><i className="bi bi-brush-fill"></i></p></button></Link>
       </div>
 
       <div className="side-navs-cellphone">
@@ -311,7 +311,7 @@ const handlePlay = (songUrl) => {
             <i className="bi bi-currency-dollar"></i>
         </Link>
         <Link to="/art" className="side-nav">
-            <i className="bi bi-currency-dollar"></i>
+            <i className="bi bi-brush-fill"></i>
         </Link>
       </div>
       <br />
