bd74da04f11ebf069ee6b33bc9d11efe088ef443
Tlotlo <114500548+butterrobot2022@users.noreply.github.com>
2025-06-06 16:14:00 +0200

    deez nuts

--- Frontend/src/components/side_navs.jsx
+++ Frontend/src/components/side_navs.jsx
@@ -1,9 +1,80 @@
+
 import React, { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import { v4 as uuidv4 } from 'uuid';
 import { FaSun, FaMoon, FaMusic, FaSave, FaChartLine, FaAngleDown, FaAngleUp } from 'react-icons/fa';
 import { useAudio } from './audio_context';
 import AssetTracker from "./asset_tracker";
+
+
+
+// Import all the songs
+import jingleBells from '../jingle_bells.mp3';
+import snowStorm from '../Snowstorm Sound Effect - Winter Storm - Blizzard.mp3';
+import love_story from '../Indila - Love Story (Piano Cover).mp3';
+import ezio_family from "../Assassin's Creed 2 OST  Jesper Kyd - Ezio's Family (Track 03).mp3";
+import hymn_for_the_weekend from '../Coldplay - Hymn For The Weekend (Lyrics).mp3';
+import daydreaming from '../Marc Wavy - Daydreaming (Official Lyric Video).mp3';
+import me_times_two from '../Raptures - Me Times Two (ft. Moav)  Electronic Pop  NCS - Copyright Free Music.mp3';
+import we_dont_talk_anymore from "../We Don't Talk Anymore我們不再交談Charlie Puth ft.Selena Gomez 中文字幕.mp3";
+import should_i_stay from '../The Clash - Should I Stay or Should I Go (Official Audio).mp3';
+import the_middle from '../Zedd, Maren Morris, Grey - The Middle (Lyric Video).mp3';
+import quiet_night from '../Tokyo Music Walker - Quiet Night.mp3';
+import feels from '../Calvin Harris - Feels (Official Video) ft. Pharrell Williams, Katy Perry, Big Sean.mp3';
+import im_good from "../David Guetta x Bebe Rexha - I'm Good (Blue) (Senses & Shadow Remix).mp3";
+import never_give_up from '../Never Give Up.mp3';
+import gravity from '../Sara Bareilles - Gravity (Official HD Video).mp3';
+import closer from '../The Chainsmokers - Closer (Lyric) ft. Halsey.mp3';
+import bloody_mary_edit from '../bloody mary (instrumental x dum dum, da-di-da) [full version] - lady gaga [edit audio].mp3';
+import waiting from '../Waiting.mp3';
+import wish_wonderland from '../Wonderland 원더랜드 OST WISH_ Wonderland is here 박보검 Park Bo-gum 배수지 Bae Suzy HanRomEnglish Lyrics.mp3';
+import welcome_to_columbia from '../Congratulations! Welcome to Columbia!.mp3';
+import 沉溺 from '../沉溺（你让我的心不再结冰）.mp3';
+import shoot_to_thrill from '../ACDC - Shoot to Thrill.mp3';
+import when_im_with_you from "../When I'm with you.mp3";
+import coffee_time from '../321Jazz - Coffee Time [ Cafe Jazz Music 2024 ].mp3';
+import coffee_lounge from '../Coffee Lounge.mp3';
+import good_vibes from '../Good Vibes.mp3';
+import iced_coffee_jazz from '../iced coffee  jazz lofi vibes (no copyright music  vlog music  royalty free music).mp3';
+import sitting_in_a_cafe from '../Sitting in a Café.mp3';
+import lex_mit_car from '../LexMITCar.mp3';
+import keep_it_lowkey from '../spotifydown.com - keep it lowkey - take your time.mp3';
+import honey_jam from '../massobeats - honey jam (royalty free lofi music).mp3';
+import floral from '../massobeats - floral (royalty free lofi music).mp3';
+import lemon_cake from '../샛별 - Lemon Cake (Royalty Free Music).mp3';
+import marshmellow from '../lukrembo - marshmallow (royalty free vlog music).mp3';
+import rose from '../lukrembo - rose (royalty free vlog music).mp3';
+import this_is_mit from '../This is MIT.mp3';
+import time_between_storms from '../Dune_ Part Two Soundtrack  A Time of Quiet Between the Storms - Hans Zimmer  WaterTower.mp3';
+import somnus_theme from '../Final fantasy XIII versus somnus theme [bOyYigKniW4].mp3';
+import your_man from '../Your Man.mp3';
+import cry_baby from '../SZA - Cry Baby (Lyrics).mp3';
+import genesis from '../Transcendence - GENESIS.mp3';
+import rewrite_the_stars from '../rewrite the stars (speed up  lyrics).mp3';
+import bloodline from '../Ariana Grande - bloodline (Official Audio).mp3';
+import ma_meilleure_enemie from '../Stromae, Pomme - “Ma Meilleure Ennemie” (from Arcane Season 2) [Official Visualizer].mp3';
+import procrastination from '../Diverseddie 舵 - Procrastination 拖延症.mp3';
+import atreides_theme from '../Atreides Theme.mp3';
+import duncan_theme from '../3m24 Duncan Arrives (Unreleased)  Dune (2021).mp3';
+import mit_hall from '../“Hall That Never Ends,” featuring the @mitlogs Written, directed, and edited by Reuben Fuchs.Check out their new album “Log Log Land,” streaming now!.mp3';
+import mit from '../mit.mp3';
+import empire_state_of_mind from '../JAY-Z - Empire State Of Mind (Lyrics) ft. Alicia Keys.mp3';
+import here_comes_the_sun from '../The Beatles - Here Comes The Sun (2019 Mix).mp3';
+import afternoon_of_konoha from '../Naruto - Afternoon of Konoha.mp3';
+import chosen from '../ilyaugust - Chosen Dreaming, Dreaming of This Moment (Official Lyric Video).mp3';
+import spin_u_round from '../spin u round.mp3';
+import feel_it from '../d4vd - Feel It.mp3';
+import mona_lisa from '../Dominic Fike - Mona Lisa (Official Audio) (1).mp3';
+import forever_star from '../Forever Star偷偷藏不住電視劇插曲 -  張洢豪Wherever you goIll surround you still動態歌詞.mp3';
+import copines from '../Aya Nakamura - Copines (Clip officiel).mp3';
+import dizzy from '../Dizzy  Joakim Karud (No Copyright Music).mp3';
+import classic from '../MKTO - Classic (Lyrics).mp3';
+import classic_slowed from '../𝙘𝙡𝙖𝙨𝙨𝙞𝙘 - 𝙈𝙆𝙏𝙊 (𝙨𝙡𝙤𝙬𝙚𝙙  𝙡𝙮𝙧𝙞𝙘𝙨).mp3';
+import sound_of_april from '../Sound of April.mp3';
+import what_are_you_waiting_for from '../d4vd - What Are You Waiting For (Lyrics).mp3';
+import a_million_colors from '../A Million Colors.mp3';
+import annas_smile from "../Anna's Smile.mp3";
+
 
 export default function SideNavs() {
   const uniqueID = uuidv4();
@@ -19,64 +90,73 @@ export default function SideNavs() {
   const [isLoading, setIsLoading] = useState(false);
   const baseURL = 'https://backend-production-c0ab.up.railway.app';
 
-  // Import song files
+  // Import song files - Fixed the undefined imports
   const songs = [
-    { name: "MIT👨‍🎓📖🚀", file: "/songs/mit.mp3" },
-    { name: "Atreides Theme ⚔️", file: "/songs/atreides_theme.mp3" },
-    { name: "Jingle Bells", file: "/songs/jingle_bells.mp3" },
-    { name: "Snow Storm", file: "/songs/snowstorm.mp3" },
-    { name: "Love Story", file: "/songs/love_story.mp3" },
-    { name: "Ezio's Family", file: "/songs/ezio_family.mp3" },
-    { name: "Hymn For The Weekend", file: "/songs/hymn_for_the_weekend.mp3" },
-    { name: "Daydreaming", file: "/songs/daydreaming.mp3" },
-    { name: "Me Times Two", file: "/songs/me_times_two.mp3" },
-    { name: "We Don't Talk Anymore", file: "/songs/we_dont_talk_anymore.mp3" },
-    { name: "Should I Stay or Should I Go", file: "/songs/should_i_stay.mp3" },
-    { name: "The Middle", file: "/songs/the_middle.mp3" },
-    { name: "Quiet Night", file: "/songs/quiet_night.mp3" },
-    { name: "Feels", file: "/songs/feels.mp3" },
-    { name: "I'm Good (Blue)", file: "/songs/im_good.mp3" },
-    { name: "Never Give Up", file: "/songs/never_give_up.mp3" },
-    { name: "Gravity", file: "/songs/gravity.mp3" },
-    { name: "Closer", file: "/songs/closer.mp3" },
-    { name: "Bloody Mary (Edit)", file: "/songs/bloody_mary_edit.mp3" },
-    { name: "Waiting 💙", file: "/songs/waiting.mp3" },
-    { name: "Wish (Wonderland) ✨🎸", file: "/songs/wish_wonderland.mp3" },
-    { name: "Welcome to Columbia!📖🚀", file: "/songs/welcome_to_columbia.mp3" },
-    { name: "沉溺（你让我的心不再结冰) 🎶🌆", file: "/songs/沉溺.mp3" },
-    { name: "Shoot to Thrill - ACDC 🤖🎸", file: "/songs/shoot_to_thrill.mp3" },
-    { name: "When I'm With You - Arcando", file: "/songs/when_im_with_you.mp3" },
-    { name: "Coffee Time ☕", file: "/songs/coffee_time.mp3" },
-    { name: "Coffee Lounge ☕", file: "/songs/coffee_lounge.mp3" },
-    { name: "Good Vibes 😌", file: "/songs/good_vibes.mp3" },
-    { name: "Iced Coffee Jazz ☕🎶", file: "/songs/iced_coffee_jazz.mp3" },
-    { name: "Sitting in a Café ☕👨‍💻", file: "/songs/sitting_in_a_cafe.mp3" },
-    { name: "Lex MIT Car 🤖🚗", file: "/songs/lex_mit_car.mp3" },
-    { name: "Keep it lowkey 🎺", file: "/songs/keep_it_lowkey.mp3" },
-    { name: "Honey Jam 🍯", file: "/songs/honey_jam.mp3" },
-    { name: "Floral 🌺💮", file: "/songs/floral.mp3" },
-    { name: "Lemon Cake 🍋🍰", file: "/songs/lemon_cake.mp3" },
-    { name: "Marshmellow 😋", file: "/songs/marshmellow.mp3" },
-    { name: "Rose 🌹", file: "/songs/rose.mp3" },
-    { name: "This is MIT 👨‍🎓📚", file: "/songs/this_is_mit.mp3" },
-    { name: "Dune: Time between storms ⌛🗡️", file: "/songs/time_between_storms.mp3" },
-    { name: "Somnus Theme 🐺🥷", file: "/songs/somnus_theme.mp3" },
-    { name: "Joji - Your Man 🦸‍♂️🦸‍♀️", file: "/songs/your_man.mp3" },
-    { name: "Cry Baby - SZA 🌃🌃", file: "/songs/cry_baby.mp3" },
-    { name: "Genesis - Jorma Kaukonen 🧑🏾‍🤝‍👩🏼👨‍💻👩‍💻", file: "/songs/genesis.mp3" },
-    { name: "Rewrite the Stars 🌃", file: "/songs/rewrite_the_stars.mp3" },
-    { name: "Bloodline - Ariana Grande 🎤", file: "/songs/bloodline.mp3" },
-    { name: "Stromae, Pomme - 'Ma Meilleure Ennemie'(from Arcane Season 2)🌃", file: "/songs/ma_meilleure_enemie.mp3" },
-    { name: "Diverseddie 舵 - Procrastination 拖延症 😌👨‍💻", file: "/songs/procrastination.mp3" },
-    { name: "Duncan's Theme 🗡️", file: "/songs/duncan_theme.mp3" },
-    { name: "MIT Hall That Never Ends 👨‍🎓🎶", file: "/songs/mit_hall.mp3" },
-    { name: "Empire State of Mind 🗽🌆", file: "/songs/empire_state_of_mind.mp3" },
-    { name: "Here Comes The Sun 🌄", file: "/songs/here_comes_the_sun.mp3" },
-    { name: "Afternoon of Konoha 🌳🌄", file: "/songs/afternoon_of_konoha.mp3" },
-    { name: "Chosen ⌛", file: "/songs/chosen.mp3" },
-    { name: "Spin U Around 🎼💙", file: "/songs/spin_u_round.mp3" },
-    { name: "Feel it 🦸‍♂️🦸‍♀️", file: "/songs/feel_it.mp3" },
-    { name: "Mona Lisa 🎨🖌️", file: "/songs/mona_lisa.mp3" }
+        { name: "MIT👨‍🎓📖🚀", file: mit },
+        { name: "Atreides Theme ⚔️", file: atreides_theme },
+        { name: "Jingle Bells", file: jingleBells },
+        { name: "Snow Storm", file: snowStorm },
+        { name: "Love Story", file: love_story },
+        { name: "Ezio's Family", file: ezio_family },
+        { name: "Hymn for The Weekend", file: hymn_for_the_weekend },
+        { name: "Daydreaming", file: daydreaming },
+        { name: "Me Times Two", file: me_times_two},
+        { name: "We Don't Talk Anymore", file: we_dont_talk_anymore },
+        { name: "Should I Stay or Should I Go", file: should_i_stay },
+        { name: "The Middle", file: the_middle },
+        { name: "Quiet Night", file: quiet_night },
+        { name: "Feels", file: feels },
+        { name: "I'm Good (Blue)", file: im_good },
+        { name: "Never Give Up", file: never_give_up },
+        { name: "Gravity", file: gravity },
+        { name: "Closer", file: closer },
+        { name: "Bloody Mary (Edit)", file: bloody_mary_edit },
+        { name: "Waiting 💙", file: waiting },
+        { name: "Wish (Wonderland) ✨🎸", file: wish_wonderland },
+        { name: "Welcome to Columbia!📖🚀", file: welcome_to_columbia },
+        { name: "沉溺（你让我的心不再结冰) 🎶🌆", file: 沉溺 },
+        { name: "Shoot to Thrill - ACDC 🤖🎸", file: shoot_to_thrill },
+        { name: "When I'm With You - Arcando", file: when_im_with_you },
+        { name: "Coffee Time ☕", file: coffee_time },
+        { name: "Coffee Lounge ☕", file: coffee_lounge },
+        { name: "Good Vibes 😌", file: good_vibes },
+        { name: "Iced Coffee Jazz ☕🎶", file: iced_coffee_jazz },
+        { name: "Sitting in a Café ☕👨‍💻", file: sitting_in_a_cafe },
+        { name: "Lex MIT Car 🤖🚗", file: lex_mit_car },
+        { name: "Keep it lowkey 🎺", file: keep_it_lowkey },
+        { name: "Honey Jam 🍯", file: honey_jam },
+        { name: "Floral 🌺💮", file: floral },
+        { name: "Lemon Cake 🍋🍰", file: lemon_cake },
+        { name: "Marshmellow 😋", file: marshmellow},
+        { name: "Rose 🌹", file: rose},
+        { name: "This is MIT 👨‍🎓📚", file: this_is_mit },
+        { name: "Dune: Time between storms ⌛🗡️", file: time_between_storms },
+        { name: "Somnus Theme 🐺🥷", file: somnus_theme },
+        { name: "Joji - Your Man 🦸‍♂️🦸‍♀️", file: your_man },
+        { name: "Cry Baby - SZA 🌃🌃", file: cry_baby },
+        { name: "Genesis - Jorma Kaukonen 🧑🏾‍🤝‍👩🏼👨‍💻👩‍💻", file: genesis },
+        { name: "Rewrite the Stars 🌃", file: rewrite_the_stars },
+        { name: "Bloodline - Ariana Grande 🎤", file: bloodline },
+        { name: "Stromae, Pomme - “Ma Meilleure Ennemie” (from Arcane Season 2)🌃", file: ma_meilleure_enemie },
+        { name: "Diverseddie 舵 - Procrastination 拖延症 😌👨‍💻", file: procrastination },
+        { name: "Duncan's Theme 🗡️", file: duncan_theme },
+        { name: "MIT Hall That Never Ends 👨‍🎓🎶", file: mit_hall },
+        { name: "Empire State of Mind 🗽🌆", file: empire_state_of_mind },
+        { name: "Here Comes The Sun 🌄", file: here_comes_the_sun },
+        { name: "Afternoon of Konoha 🌳", file: afternoon_of_konoha },
+        { name: "Chosen ⌛", file: chosen },
+        { name: "Spin U Around 🎼💙", file: spin_u_round },
+        { name: "Feel it 🦸‍♂️🦸‍♀️", file: feel_it },
+        { name: "Mona Lisa 🎨🖌️", file: mona_lisa },
+        { name: "Forever Star 🌃", file: forever_star },
+        { name: "Copines 🌳", file: copines },
+        { name: "Dizzy Joakim Karud 🎒👨‍🎓", file: dizzy },
+        { name: "Classic 😎🏖️", file: classic },
+        { name: "Classic (slowed) 🏄‍♂️", file: classic_slowed },
+        { name: "Sound of April 🌃🎧", file: sound_of_april },
+        { name: "What are you waiting for? 🏄‍♂️", file: what_are_you_waiting_for },
+        { name: "A Million Colors 🎺", file: a_million_colors },
+        { name: "Anna's Smile 🌹", file: annas_smile},
   ];
 
   // Toggle functions
@@ -140,7 +220,7 @@ export default function SideNavs() {
     return () => clearInterval(interval);
   }, []);
 
-  // Access audio context
+  // Access audio context - This is working correctly!
   const { isPlaying, currentSong, playMusic, stopMusic } = useAudio();
 
   // Function to save all songs to the backend
@@ -206,11 +286,15 @@ export default function SideNavs() {
     setTimeout(() => setSavingStatus(""), 5000);
   };
 
-  // Handle play for any song (backend or local)
-const handlePlay = (songUrl) => {
-  console.log("Playing song from URL:", songUrl);
-  playMusic(songUrl);
-};
+  // Handle play for any song (backend or local) - Fixed to use the correct file property
+  const handlePlay = (song) => {
+    console.log("Playing song:", song.name);
+    // For backend songs, use song.file (which should be the URL)
+    // For local songs, use song.file (which is the imported file)
+    const songUrl = songsFromBackend.length > 0 ? song.file : song.file;
+    console.log("Song URL:", songUrl);
+    playMusic(songUrl);
+  };
 
   // Filter songs based on search term
   const filteredSongs = songsFromBackend.length > 0 
@@ -421,18 +505,12 @@ const handlePlay = (songUrl) => {
                     {filteredSongs.map((song, index) => (
                       <li key={index} className="list-group-item d-flex justify-content-between align-items-center">
                         <span>{song.name}</span>
-                        {/* <button 
+                        <button 
                           className="btn btn-sm btn-outline-primary" 
-                          onClick={() => handlePlay(songsFromBackend.length > 0 ? song.file : song.file)}
+                          onClick={() => handlePlay(song)}
                         >
                           Play
-                        </button> */}
-                        <button 
-                        className="btn btn-sm btn-outline-primary" 
-                        onClick={() => handlePlay(song.file)}
-                      >
-                        Play
-                      </button>
+                        </button>
                       </li>
                     ))}
                   </ul>
