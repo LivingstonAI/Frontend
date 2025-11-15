8311e4e9f25ac8605006e643cc837d2966a54260
Tlotlo <114500548+butterrobot2022@users.noreply.github.com>
2025-11-15 21:04:31 +0200

    deez nuts

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -79,6 +79,7 @@ import HedgeFundTracker from './components/hedge_funds.jsx';
 import ProbabilityEngine from './components/prob_engine.jsx';
 import SnowAIBrowser from './components/browser.jsx';
 import MultiAccountAnalytics from './components/multi_account.jsx';
+import SnowAIVideos from '../components/videos.jsx';
 
 
 import Zhenya from './components/zhenya';
@@ -533,6 +534,12 @@ function App() {
             } />
 
             <Route path='/browser' element={
+              <ProtectedRoute>
+                <SnowAIBrowser />
+              </ProtectedRoute>
+            } />
+
+            <Route path='/videos' element={
               <ProtectedRoute>
                 <SnowAIBrowser />
               </ProtectedRoute>
