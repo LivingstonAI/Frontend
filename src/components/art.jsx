49fc5ad840e5b14923d6f1416a43bc51a6b861d1
Tlotlo <114500548+butterrobot2022@users.noreply.github.com>
2025-12-16 23:10:49 +0200

    Update art.jsx

--- src/components/art.jsx
+++ src/components/art.jsx
@@ -100,6 +100,7 @@ import no_batidao from '../NO BATIDÃO.mp3';
 import celebrate_alan from '../Celebrate - Alan Avry (prod. by d.higgs) (unofficial videos).mp3';
 import gods from "../NewJeans (뉴진스) 'GODS' Lyrics (Color Coded Lyrics)  League of Legends - Worlds 2023 Anthem.mp3";
 import mente_ma from '../MENTE MÁ - NAKAMA (Official Lyric Video).mp3';
+import bang_lai from '../攬佬SKAI ISYOURGOD - 八方來財  Ba Fang Lai Cai (Stacks from All Sides)動態歌詞English SubsPinyin.mp3';
 
 // Audio to be used for authentication
 import access_granted_audio from '../Access Granted Sound.mp3';
@@ -209,7 +210,8 @@ export default function Art() {
         "94": { name: "No Batidao 🇧🇷🕺", file: no_batidao },
         "95": { name: "Celebrate - Alan Avry 🦜", file: celebrate_alan },
         "96": { name: "GODS - 뉴진스", file: gods },
-        "97": { name: "MENTE MA 🏄", file: mente_ma }
+        "97": { name: "MENTE MA 🏄", file: mente_ma },
+        "98": { name: "Ba Fang Lai Cai🎧🌃", file: bang_lai },
       };
   
   // Music player states
